c3bec75a73a7ed20f16cd8b459b275c8cc5f7290
VIEN HONG <vienhong88@gmail.com>
2023-06-08 23:08:13 -0400

    Update page.js

--- app/user/page.js
+++ app/user/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div>Boring Rainny day at work trying to submit to github desktop </div>
+    <div>Thursday recovery day</div>
   )
 }
 
